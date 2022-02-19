ab6307177af5dc65d830672086be2905f91b3c78
AnaCoda <anacod@zoho.com>
2022-02-19 11:55:52 -0700

    Util for getting bus stops from city of calgary api

---
+++ utils/BusStops_API.py
@@ -0,0 +1,22 @@
+# make sure to install these packages before running:
+# pip install pandas
+# pip install sodapy
+
+import pandas as pd
+from sodapy import Socrata
+
+def connectToBusAPI():
+    # No authentication needed
+    client = Socrata("data.calgary.ca", None)
+    return client
+
+
+def getAllStopsAsDF():
+    cClient = connectToBusAPI()
+    # There are less than 8000 stops so this will get them all
+    results = cClient.get("muzh-c9qc", limit=8000)
+    # Convert to pandas DataFrame
+    results_df = pd.DataFrame.from_records(results)
+    return results_df
+
+print(getAllStopsAsDF())
